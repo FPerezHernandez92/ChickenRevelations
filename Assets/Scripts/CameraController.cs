5dde61bd53b416fe47d9bb9bd856ce0e2b2b4794
Francisco Pérez <herpefran92@correo.ugr.es>
2016-04-03 18:35:43 +0200

    Añadido dinosaurio y terreno y cámara

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	private GameObject player;
+	private Vector3 offset, offset2, offset3;
+
+	public int margin;
+
+	// Use this for initialization
+	void Start () {
+		player = transform.parent.gameObject;
+		offset = offset2 = cameraPosition();;
+		//offset3 = new Vector3(offset.x, offset.y+40, offset.z+10);
+		offset3 = new Vector3(offset.x, offset.y, offset.z);
+
+		transform.position = player.transform.position + offset;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Si el personaje entra en el margen inferior
+		if(player.transform.position.z < margin){
+			if(offset2.x == offset3.x && offset2.y < offset3.y && offset2.z < offset3.z){
+				offset2 += new Vector3(0, (float)0.4, (float)0.8);
+			}
+		}
+		else{
+			if(offset2 != offset){
+				offset2 -= new Vector3(0, (float)0.4, (float)0.8);
+			}
+		}
+
+		transform.position = player.transform.position + offset2;
+		transform.LookAt(player.transform);
+	}
+
+	private Vector3 cameraPosition(){
+		Vector3 posCamera = new Vector3();
+
+		posCamera.x = -(transform.parent.position.x - transform.position.x);
+		posCamera.y = -(transform.parent.position.y - transform.position.y);
+		posCamera.z = -(transform.parent.position.z - transform.position.z);
+	
+		return posCamera;
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+
+	public float walkSpeed = 5;
+	public float runSpeed = 15;
+	private float acceleration = 8;
+	public float gravity;
+
+
+
+	private CharacterController ccontroller;
+	private Animator anim;
+
+	void Start(){
+
+		//Manejo del personaje
+		ccontroller = GetComponent<CharacterController>();
+
+		//Sistema de animacion
+		anim = gameObject.GetComponentInChildren<Animator>();
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		Vector3 movement = Vector3.zero;
+
+		//Solo si se encuentra en el suelo, sino se le aplica gravedad
+		if(ccontroller.isGrounded){
+			movement = keyboardController();
+		}
+
+		//Gravedad
+		movement.y -= gravity * Time.deltaTime * 10;
+
+		//Animacion
+		animAdmin();
+
+		Debug.Log (movement);
+
+		ccontroller.Move(movement * Time.deltaTime); 
+	}
+
+
+	private Vector3 keyboardController(){
+
+
+		//Vector del movimiento
+		Vector3 movementVector = Vector3.zero;
+		movementVector = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));			
+
+		movementVector = (Input.GetButton("Run")) ? movementVector *= runSpeed : movementVector *= walkSpeed;
+
+		if(movementVector != Vector3.zero)
+			transform.forward = Vector3.Normalize(movementVector);
+
+		return movementVector;
+	}
+
+	private void animAdmin(){
+		/*if((CrossPlatformInputManager.GetAxis("Horizontal") != 0 || CrossPlatformInputManager.GetAxis("Vertical") != 0) ||
+			(Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)){
+			anim.SetBool("Walk", true);
+
+			if(CrossPlatformInputManager.GetButton("Yellow") || Input.GetButton("Run")){
+				if(Input.GetButton("Run"))
+					anim.SetBool("Run", true);
+			}
+			else
+				anim.SetBool("Run", false);
+
+		}
+		else{
+			anim.SetBool("Walk", false);
+			anim.SetBool("Run", false);
+		}*/
+	}
+}
