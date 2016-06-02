ff4c63ef1b6f9591f3868cf40e71fc65546dfed4
Francisco Pérez <herpefran92@correo.ugr.es>
2016-06-02 10:51:33 +0200

    el zombi muere

--- Assets/Scripts/Contrincante.cs
+++ Assets/Scripts/Contrincante.cs
@@ -26,20 +26,22 @@ public class Contrincante : MonoBehaviour {
 	void Update () {
 		//Animacion
 		animAdmin ();
-		if (navmes.SetDestination (player.transform.position)) {
-			float aux = Vector3.Distance (player.transform.position, gameObject.transform.position);
-			if (distancia > aux) {
-				estado = 2;
-				player.GetComponent <PlayerMovement>().QuitarVida();
-				int auxvida = player.GetComponent <PlayerMovement>().GetVida();
-				print ("VIDA " + auxvida);
-			} else {
-				estado = 1;
-		
+		if (estado != 3) {
+			if (navmes.SetDestination (player.transform.position)) {
+				float aux = Vector3.Distance (player.transform.position, gameObject.transform.position);
+				if (distancia > aux) {
+					estado = 2;
+					player.GetComponent <PlayerMovement> ().QuitarVida ();
+					int auxvida = player.GetComponent <PlayerMovement> ().GetVida ();
+					print ("VIDA " + auxvida);
+				} else {
+					estado = 1;
+			
+				}
 			}
+			if (vidaContrincante <= 0)
+				estado = 3;
 		}
-		if (vidaContrincante <= 0)
-			estado = 3;
 	}
 
 	private void animAdmin(){
@@ -59,6 +61,7 @@ public class Contrincante : MonoBehaviour {
 			animcontr.SetBool ("WalkC", false);
 			animcontr.SetBool ("WaitC", false);
 			animcontr.SetBool ("AtackC", false);
+			animcontr.SetBool ("DieC", true);
 		}
 	}
 
